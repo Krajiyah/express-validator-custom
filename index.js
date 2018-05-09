ebe2b261ba5fa64a387aad06661f8f6f216ed027
Krishnan Rajiyah <krajiyah@gmail.com>
2018-05-08 19:19:10 -0700

    [feat] more validation

--- index.js
+++ index.js
@@ -31,11 +31,36 @@ module.exports = expressValidator({
     valueLessThan: function(param, value) {
       return param < value;
     },
+    valueLessThanOrEqualTo: function(param, value) {
+      return param <= value;
+    },
     valueGreaterThan: function(param, value) {
       return param > value;
     },
+    valueGreaterThanOrEqualTo: function(param, value) {
+      return param >= value;
+    },
+    isObject: function(value) {
+      return !Array.isArray(value) && typeof(value) == "object";
+    },
+    isNonEmptyObject: function(value) {
+      return !Array.isArray(value) && typeof(value) == "object" && Object
+        .keys(value).length > 0;
+    },
+    isArray: function(value) {
+      return Array.isArray(value);
+    },
     isNonEmptyArray: function(value) {
       return (Array.isArray(value)) && (value.length > 0);
+    },
+    isValidString: function(param) {
+      return typeof(param) == "string";
+    },
+    isValidNumber: function(param) {
+      return typeof(param) == "number";
+    },
+    isValidUrl: function(param) {
+      return isUrl(param);
     },
     isFnTrue: function(param, fn) {
       try {
@@ -46,14 +71,13 @@ module.exports = expressValidator({
     },
     isAsyncFnTrue: function(param, fn) {
       return _promiseBoolTrue(async function() {
-        return (await fn(param)) === true;
+        try {
+          let b = await fn(param);
+          return b === true;
+        } catch (e) {
+          return false;
+        }
       });
-    },
-    isValidInteger: function(param) {
-      return typeof(param) == "number";
-    },
-    isValidUrl: function(param) {
-      return isUrl(param);
     }
   }
 });
